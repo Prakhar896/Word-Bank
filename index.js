eb1ef3725ac7c3b0c68586580dee5522fe91f0a3
Prakhar Trivedi <prakhar0706@gmail.com>
2022-01-26 03:44:47 +0000

    hi

--- index.js
+++ index.js
@@ -12,16 +12,17 @@ axios({
 .then(response => {
     if (response.status == 200) {
         const keys = Object.keys(response.data)
-
+        wordsDiv.innerHTML = ""
         keys.forEach((key, index) => {
+            console.log(key)
             const paraElem = document.createElement('p')
             paraElem.innerText = key
             const anchorElem = document.createElement('a')
             anchorElem.href = `${originURL}/word/${key}`
             anchorElem.appendChild(paraElem)
-            wordsDiv.innerHTML = ""
             bodyElem.removeChild(statusLabel)
             wordsDiv.appendChild(anchorElem)
+            console.log(wordsDiv.innerHTML)
         })
     } else {
         statusLabel.innerHTML = "An error occurred in loading words. Check console for more information."
