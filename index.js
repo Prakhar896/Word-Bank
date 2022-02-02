c9c18085a16a5ffa8d2fdd25ed3de8e4bbda2128
Prakhar Trivedi <53103894+Prakhar896@users.noreply.github.com>
2022-02-02 14:27:28 +0800

    finished word bank

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
-const wordsDiv = document.getElementById("words")
-const statusLabel = document.getElementById("statusLabel")
-const bodyElem = document.getElementById("bodyElem")
-const originURL = location.origin
+console.log("hello")
 
 axios({
     method: 'get',
@@ -9,23 +6,35 @@ axios({
     url: `${origin}/api/getWords`,
     data: {}
 })
-.then(response => {
-    if (response.status == 200) {
-        const keys = Object.keys(response.data)
-        wordsDiv.innerHTML = ""
-        keys.forEach((key, index) => {
-            console.log(key)
-            const paraElem = document.createElement('p')
-            paraElem.innerText = key
-            const anchorElem = document.createElement('a')
-            anchorElem.href = `${originURL}/word/${key}`
-            anchorElem.appendChild(paraElem)
-            bodyElem.removeChild(statusLabel)
-            wordsDiv.appendChild(anchorElem)
-            console.log(wordsDiv.innerHTML)
-        })
-    } else {
-        statusLabel.innerHTML = "An error occurred in loading words. Check console for more information."
-        console.log("Received non-200 response status code from getWords API")
-    }
-})
+    .then(response => {
+        const wordsDiv = document.getElementById("words")
+        const statusLabel = document.getElementById("statusLabel")
+        const bodyElem = document.getElementById("bodyElem")
+        const originURL = location.origin
+        console.log(response)
+        if (response.status == 200) {
+            const keys = Object.keys(response.data)
+            wordsDiv.innerHTML = ""
+            if (!keys || keys == [] || response.data == {}) {
+                statusLabel.innerHTML = "No words stored."
+            } else {
+                bodyElem.removeChild(statusLabel)
+                keys.forEach((key, index) => {
+                    const anchorElem = document.createElement("a")
+                    anchorElem.href = `${originURL}/word/${key}`
+
+                    const paraElem = document.createElement("p")
+                    paraElem.innerHTML = key
+
+                    anchorElem.appendChild(paraElem)
+                    wordsDiv.appendChild(anchorElem)
+                })
+            }
+        } else {
+            statusLabel.innerHTML = "An error occurred in loading words. Check console for more information."
+            console.log("Received non-200 response status code from getWords API")
+        }
+    })
+    .catch(err => {
+        console.log(err)
+    })
